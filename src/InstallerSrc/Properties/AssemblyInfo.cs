f24e72a46cc97fd16ae5acaa73393bc4e1597ab1
Bogdancev <sergiy.bogdancev@gmail.com>
2017-09-03 21:41:51 +0200

    Fixed 2 spelling mistakes.

--- src/InstallerSrc/Properties/AssemblyInfo.cs
+++ src/InstallerSrc/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HLSourceUkr")]
-[assembly: AssemblyDescription("Українізатор HL:Source")]
+[assembly: AssemblyDescription("Українізатор Half-Life: Source від STS UA")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("STS")]
 [assembly: AssemblyProduct("HLSourceUkr")]
